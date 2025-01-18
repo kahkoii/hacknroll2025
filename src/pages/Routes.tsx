24e84c0ed845824a8e5ef15d3e51618635de9135
Kah Ho <sweekahho@gmail.com>
2025-01-18 13:56:10 +0800

    Create default home page and temp treasure page

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Home/Home'
+import Treasure from './Treasure/Treasure'
 
 const App: React.FC = () => (
 	<Router>
 		<Routes>
 			{/* Public Pages */}
 			<Route path="/*" element={<Home />} />
+			<Route path="/treasure" element={<Treasure />} />
 		</Routes>
 	</Router>
 )
