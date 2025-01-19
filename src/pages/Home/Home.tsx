27cf6b5d46131575bd21e6501f4b65b6f0f475d1
KennethTeo2002 <kenneth.teo2002@gmail.com>
2025-01-19 12:07:30 +0800

    Add among us background

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,139 @@
 import { Flex, Checkbox, Text, Image, Spinner } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
+import Particles from 'react-tsparticles'
+import { loadFull } from 'tsparticles'
 import img from '/images/captcha.svg'
 import { useEffect, useState } from 'react'
-import '/src/pages/background.css';
+import '/src/pages/background.css'
 
+const particlesOptions = {
+	fpsLimit: 60,
+	particles: {
+		groups: {
+			z5000: {
+				number: {
+					value: 70,
+				},
+				zIndex: {
+					value: 5000,
+				},
+			},
+			z7500: {
+				number: {
+					value: 30,
+				},
+				zIndex: {
+					value: 75,
+				},
+			},
+			z2500: {
+				number: {
+					value: 50,
+				},
+				zIndex: {
+					value: 25,
+				},
+			},
+			z1000: {
+				number: {
+					value: 40,
+				},
+				zIndex: {
+					value: 10,
+				},
+			},
+		},
+		number: {
+			value: 200,
+			density: {
+				enable: false,
+				area: 800,
+			},
+		},
+		color: {
+			value: '#fff',
+		},
+		shape: {
+			type: 'circle',
+		},
+		opacity: {
+			value: { min: 0.1, max: 1 },
+		},
+		size: {
+			value: 3,
+		},
+		move: {
+			angle: { value: 10, offset: 0 },
+			enable: true,
+			speed: 5,
+			direction: 'right',
+			straight: true,
+			outModes: 'out',
+		},
+		zIndex: { value: 5, opacityRate: 0.5 },
+	},
+	interactivity: {
+		events: {
+			onClick: { enable: true, mode: 'push' },
+			resize: true,
+		},
+		modes: {
+			push: {
+				quantity: 4,
+				groups: ['z5000', 'z7500', 'z2500', 'z1000'],
+			},
+		},
+	},
+	background: {
+		color: '#000000',
+	},
+	emitters: {
+		position: { y: 55, x: -30 },
+		rate: { delay: 7, quantity: 1 },
+		particles: {
+			shape: {
+				type: 'images',
+				options: {
+					images: [
+						{
+							src: 'https://particles.js.org/images/amongus_blue.png',
+							width: 205,
+							height: 267,
+						},
+						{
+							src: 'https://particles.js.org/images/amongus_cyan.png',
+							width: 207,
+							height: 265,
+						},
+						{
+							src: 'https://particles.js.org/images/amongus_green.png',
+							width: 204,
+							height: 266,
+						},
+						{
+							src: 'https://particles.js.org/images/amongus_red.png',
+							width: 204,
+							height: 267,
+						},
+					],
+				},
+			},
+			size: { value: 40 },
+			move: {
+				speed: 10,
+				outModes: {
+					default: 'destroy',
+					left: 'none',
+				},
+				straight: true,
+			},
+			rotate: {
+				value: { min: 0, max: 360 },
+				animation: { enable: true, speed: 10, sync: true },
+			},
+		},
+	},
+}
 
 const Home: React.FC = () => {
 	const [clicked, setClicked] = useState(false)
@@ -12,16 +142,18 @@ const Home: React.FC = () => {
 	const [blueIndex, setBlueIndex] = useState(0)
 	const navigate = useNavigate()
 
-	// Slowly highlight text after checkbox is clicked
 	useEffect(() => {
 		if (clicked) {
 			setTimeout(() => {
 				setBlueIndex(blueIndex + 10)
 				setHereColour('rgb(0, 0, ' + blueIndex + ')')
-				console.log(blueIndex)
 			}, 300)
 		}
-	}, [hereColour])
+	}, [clicked, blueIndex])
+
+	const particlesInit = async (engine: any) => {
+		await loadFull(engine) // Load the full tsParticles engine
+	}
 
 	return (
 		<Flex
@@ -30,17 +162,29 @@ const Home: React.FC = () => {
 			alignItems="center"
 			justifyContent="center"
 		>
-			<div className="background-container">
-				<div className="shape"></div>
-				<div className="shape"></div>
-				<div className="shape"></div>
-			</div>
+			{/* Particles Background */}
+			<Particles
+				id="tsparticles"
+				init={particlesInit}
+				options={particlesOptions}
+				style={{
+					position: 'absolute',
+					top: 0,
+					left: 0,
+					width: '100%',
+					height: '100%',
+					zIndex: -1, // Ensures particles appear in the background
+				}}
+			/>
+
+			{/* Main Content */}
 			<Flex
 				flexDir="row"
 				width="320px"
 				border="2px solid #40376E"
 				borderRadius="10px"
 				bgColor="white"
+				zIndex={1}
 				justifyContent="space-between"
 				boxShadow="lg"
 			>
@@ -72,7 +216,7 @@ const Home: React.FC = () => {
 						color={hereColour}
 					>
 						here
-					</Text>{' '}
+					</Text>
 					<Text marginLeft="4px">to verify</Text>
 				</Flex>
 				<Flex
@@ -95,6 +239,7 @@ const Home: React.FC = () => {
 				marginTop="20px"
 				color="green.500"
 				visibility={showMessage ? 'initial' : 'hidden'}
+				zIndex={1}
 			>
 				Looks an awful lot like a bot... more tests required!
 			</Text>
