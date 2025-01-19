a9a92a31ad0b3a4caf8c9d474120512811dca65c
KennethTeo2002 <kenneth.teo2002@gmail.com>
2025-01-19 11:44:35 +0800

    Remobe route

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Home/Home'
-import Treasure from './Treasure/Treasure'
 import CoinFlip from './CoinFlip/CoinFlip'
 import MathProblemGame from './MathProblemGame/MathProblemGame'
 import Captcha from './Captcha/Captcha'
-import AimTrainer from './AimTrainer/AimTrainer'
-
 const App: React.FC = () => (
 	<Router>
 		<Routes>
@@ -14,9 +11,7 @@ const App: React.FC = () => (
 			<Route path="/*" element={<Home />} />
 			<Route path="/coinflip" element={<CoinFlip />} />
 			<Route path="/mathproblemgame" element={<MathProblemGame />} />
-			<Route path="/treasure" element={<Treasure />} />
 			<Route path="/captcha" element={<Captcha />} />
-			<Route path="/aimtrainer" element={<AimTrainer />} />
 		</Routes>
 	</Router>
 )
